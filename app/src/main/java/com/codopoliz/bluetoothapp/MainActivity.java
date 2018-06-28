7f72ffe7af6739ddd8f1426bdeff676e83ab3e1e
USER <tanmoy_india@yahoo.com>
2018-06-28 12:54:54 +0530

    test

--- app/src/main/java/com/codopoliz/bluetoothapp/MainActivity.java
+++ app/src/main/java/com/codopoliz/bluetoothapp/MainActivity.java
@@ -46,10 +46,9 @@ public class MainActivity extends Activity {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent
-                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                mDeviceList.add(device.getName() + "\n" + device.getAddress());
-                Log.i("BT", device.getName() + "\n" + device.getAddress());
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mDeviceList.add(device.getName() + "\n\n" + device.getAddress());
+                Log.i("BT", device.getName() + "\n\n" + device.getAddress());
                 listView.setAdapter(new ArrayAdapter<String>(context,
                         android.R.layout.simple_list_item_1, mDeviceList));
             }
